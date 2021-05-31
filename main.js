a79766e24a82532c13bfb6ddb4f1cd06d40e0115
Ganza Ntambuko <ganzantambuko@gmail.com>
2021-05-31 13:08:09 +0200

    initial

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
+var video = document.querySelector('#player');
+
+for (var i = 0; i < video.textTracks.length; i++) {
+   video.textTracks[i].mode = 'hidden';
+}
+
+
+
+
+
+
 
 const menuToggle = document.querySelector('.toggle')
 const showcase = document.querySelector('.showcase')
